7c663b65d3ac5b7496b0d22919df24c247827826
PaulDuxblah <potus10120@hotmail.fr>
2019-09-29 13:08:30 +0200

    Delete useless file

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,11 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-// require('../css/app.css');
 import '../css/app.scss';
 import '../css/main.scss';
 import '../css/partials/nav.scss';
 import '../css/partials/lol/champion.scss';
+
 import './images.js';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
