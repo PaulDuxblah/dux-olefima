0840ba794f5d767f45920fffbcd3aec0ba2e54f3
PaulDuxblah <potus10120@hotmail.fr>
2019-09-29 12:57:11 +0200

    Fix Symfony and Encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,19 @@
-// SCSS
-require('../css/app.scss');
-require('../css/main.scss');
-require('../css/partials/nav.scss');
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
 
-// JS
-require('./images.js');
+// any CSS you require will output into a single css file (app.css in this case)
+// require('../css/app.css');
+import '../css/app.scss';
+import '../css/main.scss';
+import '../css/partials/nav.scss';
+import '../css/partials/lol/champion.scss';
+import './images.js';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
